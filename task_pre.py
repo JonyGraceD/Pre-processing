cbde05a4d08f81959d0e1c9b64c6c869e76147b5
Jony Grace D <144801981+JonyGraceD@users.noreply.github.com>
2023-10-03 10:19:13 +0530

    Add files via upload

---
+++ task_pre.py
@@ -0,0 +1,22 @@
+# data preprocessing
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel("report(2).xlsx", keep_default_na=False, header=None)
+# pre processing
+#removing rows from head or tail
+df.drop(df.head(2).index, inplace=True)
+df.drop(df.tail(1).index, inplace=True)
+
+#removing Nan(Not a number) values (if any) 
+df.replace("", np.NaN, inplace=True)
+
+#removing  all empty columns and rows
+df.dropna(how="all", axis=1, inplace=True) #axis=1--> columns
+df.dropna(how="all", axis=0, inplace=True) #axis=0--> rows
+#reset index and header 
+df.reset_index(inplace=True)
+df.columns = np.arange(0, len(df.columns))
+
+print(df)
+df.to_csv("report(2).csv", header=None, index=None) #output into a csv file
